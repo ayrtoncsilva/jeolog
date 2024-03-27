51b4965f4c18fe3fda0cfa47410e92e0467102c0
Ayrton <ayrtoncsilvadev@gmail.comm>
2024-03-27 18:06:43 -0300

    v2

--- public/script.js
+++ public/script.js
@@ -1,6 +1,55 @@
-<script>
-function enviarFormulario(event) {
-            event.preventDefault()
-            
-}
-</script>
+const form = document.querySelector("#form");
+const inputCpf = document.getElementById("cpf");
+
+document.addEventListener("DOMContentLoaded", function() {
+    const inputs = document.querySelectorAll('.my-form input');
+    inputs.forEach(function(input) {
+        input.addEventListener('input', function() {
+            input.value = input.value.toUpperCase()
+        })
+    })
+});
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    try {
+        const formData = new FormData(form);
+        const response = await fetch('/submit', {
+            method: 'POST',
+            body: formData
+        })
+
+        if (!response.ok) {
+            throw new Error("Erro ao enviar os dados ao servidor");
+        }
+
+        const responseData = await response.text();
+        console.log('Resposta do servidor:', responseData);
+    } catch (error) {
+        console.error('Erro:', error);
+    }
+});
+
+inputCpf.addEventListener('input', (event) => {
+    let cpf = inputCpf.value.replace(/\D/g, '');
+
+    if(cpf.length >11) {
+        cpf = cpf.substring(0,11)
+    }
+
+    if (cpf.length > 3) {
+        
+        cpf = cpf.substring(0, 3) + '.' + cpf.substring(3)
+    }
+    if (cpf.length > 7) {
+        cpf = cpf.substring(0, 7) + '.' + cpf.substring(7); 
+    }
+
+    if (cpf.length > 11) {
+        cpf = cpf.substring(0, 11) + '-' + cpf.substring(11); 
+    }
+
+    inputCpf.value = cpf
+});
+
